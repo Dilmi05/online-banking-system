4d8181c12d60cea28cb407796958232b4daf96fc
Dilmi Amandha <amandhadilmi@gmail.com>
2026-03-13 21:26:18 +0530

    Created DBConnection.java

---
+++ src/module-info.java
@@ -0,0 +1,5 @@
+module Simple.Online.Banking.System {
+    requires java.sql; // Add this line to allow SQL packages
+    requires javafx.controls; // if you use JavaFX
+    requires javafx.fxml;     // if you use FXML
+}
