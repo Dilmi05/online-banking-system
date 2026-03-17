13e7db181c75b9a6c7d8ed566bf106de1cfbd3ee
Dilmi Amandha <amandhadilmi@gmail.com>
2026-03-17 20:47:44 +0530

    Updated BankAccount.java

--- src/Model/BankAccount.java
+++ src/Model/BankAccount.java
@@ -1,4 +1,49 @@
-package Model;
+package model;
 
 public class BankAccount {
+    private String accountHolder;
+    private String accountNumber;
+    private double balance;
+
+    public BankAccount(String accountHolder, String accountNumber, double balance) {
+        this.accountHolder = accountHolder;
+        this.accountNumber = accountNumber;
+        this.balance = balance;
+    }
+
+    public void deposit(double amount) {
+        if (amount > 0) {
+            balance += amount;
+            System.out.println("Deposited: " + amount);
+        } else {
+            System.out.println("Invalid amount!");
+        }
+    }
+
+    public void withdraw(double amount) {
+        if (amount > 0 && amount <= balance) {
+            balance -= amount;
+            System.out.println("Withdrawn: " + amount);
+        } else {
+            System.out.println("Insufficient balance or invalid amount!");
+        }
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public String getAccountHolder() {
+        return accountHolder;
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void displayAccountInfo() {
+        System.out.println("Account Holder: " + accountHolder);
+        System.out.println("Account Number: " + accountNumber);
+        System.out.println("Balance: " + balance);
+    }
 }
